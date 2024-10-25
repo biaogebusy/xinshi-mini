9050a7fdb8fc4f75f44bf3d75db20b283d9db111
biaogebusy <349255833@qq.com>
2024-10-25 22:47:56 +0800

    update api

--- src/utils/config.js
+++ src/utils/config.js
@@ -9,20 +9,20 @@ export let REQUEST_BASE_URL = '';
 switch (envVersion) {
 	// 开发版
 	case 'develop':
-		REQUEST_BASE_URL = `https://self.zhaobg.com`;
+		REQUEST_BASE_URL = `https://builder.design`;
 		break;
 
 	// 体验版
 	case 'trial':
-		REQUEST_BASE_URL = `https://self.zhaobg.com`;
+		REQUEST_BASE_URL = `https://builder.design`;
 		break;
 
 	// 正式版
 	case 'release':
-		REQUEST_BASE_URL = `https://www.bqshare.com`;
+		REQUEST_BASE_URL = `https://builder.design`;
 		break;
 	default:
-		REQUEST_BASE_URL = `https://www.bqshare.com`;
+		REQUEST_BASE_URL = `https://builder.design`;
 		break;
 }
 
